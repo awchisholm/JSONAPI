c3a3c45702166614553c6a73a0b7bec0918581ba
Andrew Chisholm <andrew.chisholm@hsdc.ac.uk>
2022-07-11 10:21:07 +0100

    save

--- JSONAPI.py
+++ JSONAPI.py
@@ -3,6 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return json.dumps({'name': 'alice',
-                       'email': 'alice@outlook.com'})
+    return json.dumps({'name': 'alice', 'email': 'alice@outlook.com'})
 app.run()
